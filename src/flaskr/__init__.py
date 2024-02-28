a02e63a072bf4744595a5dc52e6e84dddd1eac2d
Birdbh <birdlkjlkj123@gmail.com>
2024-02-28 09:54:12 -0800

    Initialize Web Application with __init__.py Fixes #66 by adding the creation of the web-app and a landing page for the login.html

---
+++ src/flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask
+
+from . import auth
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    #configuration is pulled from Flask Documentation
+    app.config.from_mapping(
+        SECRET_KEY='test_change_later',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    app.config.from_pyfile('config.py', silent=True)
+
+    #ensure the instance folder exists (Based on Turoial directions)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.register_blueprint(auth.bp)
+
+    return app
+        
+
+    
