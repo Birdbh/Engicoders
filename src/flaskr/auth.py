1c6259f1620dbc8cfb3c06075d1c2c7527790976
Birdbh <birdlkjlkj123@gmail.com>
2024-03-01 13:04:58 -0800

    Write login() functionality with flask web application for users to check username and password with SQlite database for a match Fixes #60

--- src/flaskr/auth.py
+++ src/flaskr/auth.py
@@ -10,5 +10,28 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
-    #Functionality goes here
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        #we still need our database developed in the previous section to be able to check the user's credentials
+        #db = get_db()
+        '''
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+        '''
+
     return render_template('auth/login.html')
