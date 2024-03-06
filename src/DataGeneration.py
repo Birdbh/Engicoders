e5fcbf43163b0bbb07c19b3e80d73275626affaf
Birdbh <birdlkjlkj123@gmail.com>
2024-03-05 20:29:51 -0800

    Create DataGeneration class attributes and test object creation with unit test. Fixes #109

--- src/DataGeneration.py
+++ src/DataGeneration.py
@@ -1,10 +1,17 @@
 import requests
 from datetime import datetime, timedelta
 
+#Documentation
+#https://www.mathworks.com/help/thingspeak/readdata.html#mw_25e0bdab-cafa-48d4-84f5-5fce55aa281b
+
 class DataGeneration:
     def __init__(self, channel, time_increment):
         self.channel = channel
         self.time_increment = time_increment
+    
+    def request_data(self):
+
 
 
 print(requests.get('https://api.thingspeak.com/channels/9/feeds.json').json())
+
