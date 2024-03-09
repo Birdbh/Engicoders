e7071dcf4298607028d8d1ffce0aed4802c16d75
Birdbh <birdlkjlkj123@gmail.com>
2024-03-08 19:55:19 -0800

    Include Function in DataGeneration class that will allow functionality for the creation for sensors and getting data Fixes #110

--- tests/test_DataGeneration.py
+++ tests/test_DataGeneration.py
@@ -1,4 +1,5 @@
 import pytest
+from datetime import datetime, timedelta
 
 import sys
 sys.path.append("src")
@@ -10,7 +11,7 @@ def test_create_data_generation():
     assert data_generation.channel_id == 1
     assert data_generation.time_increment == 5
     assert data_generation.field_number == 1
-    assert data_generation.start_date == "2021-01-01 00:00:00"
+    assert data_generation.start_date == datetime.strptime("2021-01-01 00:00:00", '%Y-%m-%d %H:%M:%S')
     assert str(data_generation) == "DataGeneration: CHANNEL_ID:1, TIME_INCREMENT:5, FIELD_NUMBER:1, START_DATE:2021-01-01 00:00:00"
 
 def test_setters_and_getters():
@@ -23,3 +24,11 @@ def test_setters_and_getters():
     assert data_generation.get_time_increment() == 10
     assert data_generation.get_field_number() == 2
     assert data_generation.get_start_date() == "2021-01-02 00:00:00"
+
+def test_fetch_thingspeak_data():
+    data_generation = DataGeneration(5, 20, 1, "2023-03-01 00:00:00")
+    
+    with pytest.raises(Exception) as e:
+        data_generation.fetch_thingspeak_data()
+
+    assert str(e.value) == "Thinkspeak request status code not equal to 200, verify channel & field id"
