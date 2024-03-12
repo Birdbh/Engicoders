7fc85279d2848a512458babab31ca74fa1e178fa
JamesShadowfyre <88523672+JamesShadowfyre@users.noreply.github.com>
2024-03-12 09:02:20 -0700

    updated tests

--- src/flaskr/register.py
+++ src/flaskr/register.py
@@ -3,18 +3,26 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from flask import Flask, render_template
-import os
+from db import get_db
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    #email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        user = username
+        if(len(username) > 0):
+            self.username = username
+            return True
 
-    def validate_email(self, email):
-        user = email
+    def validate_password(self, password, password2):
+        if(password == password2):
+            self.password = password
+            return True
+    def register(self, username, password, password2): 
+        db = get_db()
+        if(self.validate_username(self, username) and (self.validate_password(self, password, password2))):
+            db.execute("INSERT INTO user (Username, Password) values (?,?)", (self.username, self.password))
--- tests/test_register.py
+++ tests/test_register.py
@@ -1,3 +1,4 @@
+import pytest
 import sys
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
@@ -6,15 +7,26 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask import Flask, render_template
 sys.path.append("src")
 import flaskr.register as register
+import flaskr.db as dbClass
 
 
-app = Flask(__name__,template_folder='templates')
-app.config['SECRET_KEY'] = SECRET_KEY='test_change_later'
-@app.route('/')
 
-def index(): 
-    form = register.RegistrationForm()
-    return render_template('register.html', form = form)
+# app = Flask(__name__,template_folder='templates')
+# app.config['SECRET_KEY'] = SECRET_KEY='test_change_later'
+# @app.route('/')
 
-if __name__ == '__main__': 
-    app.run() 
+# def index(): 
+#     form = register.RegistrationForm()
+#     return render_template('register.html', form = form)
+
+def test_register(app):
+    with app.app_context:
+        register.RegistrationForm.register("Username", "Test", "Test")
+        db = dbClass.get_db()
+        assert db.execute("SELECT username from user") == "Username"
+
+
+
+
+# if __name__ == '__main__': 
+#     app.run() 
