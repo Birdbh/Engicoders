e4c54ec6f97060713b0d947c29a79462d4b58404
Birdbh <birdlkjlkj123@gmail.com>
2024-03-14 12:51:02 -0700

    updated work with time modes, but still runing into problems with prediction

--- src/Prediction.py
+++ src/Prediction.py
@@ -39,19 +39,25 @@ class DataPrediction:
 import sklearn as sk
 import numpy as np
 import datetime as dt
+from DataGeneration import DataGeneration
+from sensors.sensor import Sensor
 
 class DataPrediction:
-    def __init__(self, sensor, prediction_timeframe,model_name):
-        self.prediction_timeframe = prediction_timeframe
-        self.X = sensor.value.keys()
-        self.Y = sensor.value.values()
+    def __init__(self, sensor, prediction_end_date, model_name):
+        self.X = list(sensor.value.keys())
+        self.prediction_intervals = self.X[-1] - self.X[-2]
+        self.prediction_start_date = self.X[-1] + self.prediction_intervals
+        self.prediction_end_date = prediction_end_date
+
+        self.Y = list(sensor.value.values())
         self.X_future = None
         self.forcasted_values = sensor.forcasted_values
+
         self.model = self.select_model(model_name)
 
-    def select_model(model_name):
+    def select_model(self, model_name):
         if model_name == 'linear_regression':
-            return sk.LinearRegression()
+            return sk.linear_model.LinearRegression()
         elif model_name == 'random_forest':
             return sk.RandomForestRegressor()
         elif model_name == 'svm':
@@ -68,11 +74,15 @@ class DataPrediction:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 3eec899 (updated work with time modes, but still runing into problems with prediction)
         number_past_entries = len(self.X)
         number_of_future_entries = len(self.X_future)
 
         prediction_times = [i for i in range(number_past_entries, number_past_entries + number_of_future_entries)]
 
+<<<<<<< HEAD
         self.forcasted_values = self.model.predict(np.array(prediction_times).reshape(-1,1))
 
     def train_model(self):
@@ -154,6 +164,9 @@ plt.show()
     def re_format_data(self):
         self.X = np.array([date.toordinal() for date in self.X]).reshape(-1, 1)
         self.Y = np.array(self.Y)
+=======
+        self.forcasted_values = self.model.predict(self.X_future)
+>>>>>>> 3eec899 (updated work with time modes, but still runing into problems with prediction)
 
     def train_model(self):
         self.model.fit(self.X, self.Y)
@@ -162,19 +175,37 @@ plt.show()
 =======
 
     def set_prediction_timeframe(self):
-        start_prediction_date = max(self.X + dt.timedelta(days=1))
 
-        time_increment = self.X[-1] - self.X[-2]
+        current_date = self.prediction_start_date
 
-        future_dates = [start_prediction_date]
+        future_dates = [current_date]
 
-        while start_prediction_date + time_increment <= self.prediction_timeframe:
-            start_prediction_date += time_increment
-            future_dates.append(start_prediction_date)
+        while current_date + self.prediction_intervals <= self.prediction_end_date:
+            current_date += self.prediction_intervals
+            future_dates.append(current_date)
 
-        self.X_future = np.array([date.toordinal() for date in future_dates]).reshape(-1, 1)
+        self.X_future = future_dates
 
     def convert_prediction_to_dict(self):
         self.forcasted_values = dict(zip(self.X_future, self.forcasted_values))
 
+<<<<<<< HEAD
 >>>>>>> b363158 (Added code to control prediction making)
+=======
+
+time_series = DataGeneration(9, 10, 1, '2024-03-13 00:00:00').get_time_series()
+
+sensor = Sensor(1, 'Temperature Sensor', 'A sensor that measures temperature', 'float', time_series)
+
+# print(sensor.value.keys())
+predict = DataPrediction(sensor, dt.datetime(2024, 3, 17, 0, 0, 0), 'linear_regression')
+
+# predict.set_prediction_timeframe()
+# predict.train_model()
+# predict.predict()
+# predict.convert_prediction_to_dict()
+
+# print(predict.forcasted_values)
+
+
+>>>>>>> 3eec899 (updated work with time modes, but still runing into problems with prediction)
