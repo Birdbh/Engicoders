5b38cc69f9c81a629da178f1ca3a09fa589cbd2d
Birdbh <birdlkjlkj123@gmail.com>
2024-03-14 10:18:44 -0700

    Create Prediction class that will take a sensor object and fill the prediction field based on a set of possible prediction algorithms. Fixes #133

--- src/Prediction.py
+++ src/Prediction.py
@@ -1,9 +1,14 @@
 import sklearn as sk
+import numpy as np
 
 class DataPrediction:
     def __init__(self, sensor):
-        self.sensor = sensor
-    
+        self.X = sensor.value.keys()
+        self.Y = sensor.value.values()
+        self.X_future = None
+        self.forcasted_values = sensor.forcasted_values
+        self.model = None
+
     def select_model(self, model_name):
         if model_name == 'linear_regression':
             self.model = sk.LinearRegression()
@@ -15,4 +20,12 @@ class DataPrediction:
             raise ValueError("Invalid model name. Please choose from 'linear_regression', 'random_forest', or 'svm'.")
 
     def predict(self):
+        self.forcasted_values = self.model.predict(self.X)
         return self.model.predict(self.data)
+    
+    def re_format_data(self):
+        self.X = np.array([date.toordinal() for date in self.X]).reshape(-1, 1)
+        self.Y = np.array(self.Y)
+
+    def train_model(self):
+        self.model.fit(self.X, self.Y)
