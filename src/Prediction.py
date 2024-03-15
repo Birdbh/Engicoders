37a95f8290393e06c38ab3b285df4c18a390db1c
Birdbh <birdlkjlkj123@gmail.com>
2024-03-15 15:39:33 -0700

    Create Prediction class that will take a sensor object and fill the prediction field based on a set of possible prediction algorithms. Fixes #133

--- src/Prediction.py
+++ src/Prediction.py
@@ -5,12 +5,14 @@ from DataGeneration import DataGeneration
 from sensors.sensor import Sensor
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+import pandas as pd
+from pmdarima import auto_arima
+from sensors.Cleanser import cleanser
 
 
 #Create a class that will predict future values of a sensor
 #The class will take in a sensor object and a prediction end date
 #The class will calculate the prediction intervals and the prediction start date from the sensor object values
-#
 
 
 class DataPrediction:
@@ -22,42 +24,13 @@ class DataPrediction:
         self.prediction_end_date = prediction_end_date
 
         self.Y = list(sensor.value.values())
+
         self.X_future = None
         self.forcasted_values = sensor.forcasted_values
 
-        self.model = self.select_model(model_name)
+        self.model = None
 
-    def select_model(self, model_name):
-        if model_name == 'linear_regression':
-            return lm.LinearRegression()
-        elif model_name == 'random_forest':
-            return lm.RandomForestRegressor()
-        elif model_name == 'svm':
-            return lm.SVR()
-        else:
-            raise ValueError("Invalid model name. Please choose from 'linear_regression', 'random_forest', or 'svm'.")
-
-    def predict(self):
-        number_past_entries = len(self.X)
-        number_of_future_entries = len(self.X_future)
-
-        prediction_times = [i for i in range(number_past_entries, number_past_entries + number_of_future_entries)]
-
-        self.forcasted_values = self.model.predict(np.array(prediction_times).reshape(-1,1))
-
-    def train_model(self):
-        train_times = np.array([i for i in range(len(self.X))]).reshape(-1,1)
-
-        x = np.nan_to_num(train_times, copy=True, nan=0.0, posinf=0.0, neginf=0.0).astype(np.float)
-
-        y = np.nan_to_num(np.array(self.Y).reshape(-1,1), copy=True, nan=0.0, posinf=0.0, neginf=0.0)
-
-        for i in range(0,len(y)):
-            if y[i] == None:
-                y[i] = (y[i-1].astype(np.float)+y[i+1].astype(np.float))/2
-
-        self.model.fit(x.reshape(-1,1), y.reshape(-1,1))
-
+    
     def set_prediction_timeframe(self):
 
         current_date = self.prediction_start_date
@@ -70,44 +43,24 @@ class DataPrediction:
 
         self.X_future = future_dates
 
+    def predict(self):
+
+        predictions = self.model.forecast(steps=len(self.X_future))
+
+        self.forcasted_values = predictions.to_list()
+
+    def train_model(self):
+
+        df = pd.DataFrame(data=self.Y, index=self.X, columns=['Value'])
+
+        auto_arima_model = auto_arima(df, seasonal=False, stepwise=True, suppress_warnings=True, error_action="ignore", max_order=None, trace=True)
+
+        best_order = auto_arima_model.order
+
+        model = ARIMA(df, order=best_order)
+        fitted_model = model.fit()
+
+        self.model = fitted_model
+
     def convert_prediction_to_dict(self):
         self.forcasted_values = dict(zip(self.X_future, self.forcasted_values))
-
-
-time_series = DataGeneration(9, 30, 1, '2024-03-13 00:00:00').get_time_series()
-
-sensor = Sensor(1, 'Temperature Sensor', 'A sensor that measures temperature', 'float', time_series)
-
-predict = DataPrediction(sensor, dt.datetime(2024, 3, 17, 0, 0, 0), 'linear_regression')
-
-predict.set_prediction_timeframe()
-predict.train_model()
-predict.predict()
-predict.convert_prediction_to_dict()
-
-dates1 = list(sensor.value.keys())
-dates2 = list(predict.forcasted_values.keys())
-
-values1 = list(sensor.value.values())
-
-for i in range(0,len(values1)):
-    if values1[i] == None:
-        values1[i] = (float(values1[i-1])+float(values1[i+1]))/2
-
-values2 = list(predict.forcasted_values.values())
-             
-
-plt.figure(figsize=(10, 5))  # Adjust the figure size as needed
-plt.plot(dates1, values1, label='Dataset 1')  # Plot the first dataset
-plt.plot(dates2, values2, label='Dataset 2', linestyle='--')  # Plot the second dataset with a different style
-
-# Formatting the plot
-plt.xlabel('Date')  # Set x-axis label
-plt.ylabel('Value')  # Set y-axis label
-plt.title('Comparison of Two Datasets')  # Set title
-plt.legend()  # Show legend to differentiate the datasets
-plt.grid(True)  # Show grid for better readability
-plt.xticks(rotation=45)  # Rotate dates for better readability
-plt.tight_layout()  # Adjust layout to make room for the rotated date labels
-
-plt.show() 
