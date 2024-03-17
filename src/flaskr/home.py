522551591fbc87ab7f0c4eae2cbdfd946cbda9fa
Birdbh <birdlkjlkj123@gmail.com>
2024-03-17 16:01:13 -0700

    Create Prediction class that will take a sensor object and fill the prediction field based on a set of possible prediction algorithms Fixes #145

--- src/flaskr/home.py
+++ src/flaskr/home.py
@@ -3,19 +3,20 @@ from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 import sys
 sys.path.append("src")
-from sensors.sensor import Sensor
+from sensors.sensor.sensor import Sensor
 from DataGeneration import DataGeneration
 
 
-bp = Blueprint('home', __name__, url_prefix='/home')
+bp = Blueprint('main', __name__, url_prefix='/main')
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/home', methods=('GET', 'POST'))
 def home():
+
     if request.method == 'POST':
         
-        channel_id = request.form.get('channel_id')
-        time_increment = request.form.get('time_increment')
-        field_number = request.form.get('field_number')
+        channel_id = int(request.form.get('channel_id'))
+        time_increment =  int(request.form.get('time_increment'))
+        field_number =  int(request.form.get('field_id'))
         start_date = request.form.get('start_date')
 
         # Create DataGeneration
@@ -40,7 +41,7 @@ def home():
         values = [value for value in time_series_data.values()]
 
         
-        return redirect(url_for('home.home'))
+        return redirect(url_for('main.home'))
     else:
         
-        return render_template('home.html')
+        return render_template('main/home.html')
