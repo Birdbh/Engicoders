579a421339e1fa46433ddc5e65aafbf83367b239
Birdbh <birdlkjlkj123@gmail.com>
2024-03-17 10:19:19 -0700

    Full testing of prediction method

--- src/Prediction.py
+++ src/Prediction.py
@@ -5,6 +5,7 @@ from DataGeneration import DataGeneration
 from sensors.sensor import Sensor
 import matplotlib.pyplot as plt
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import pandas as pd
 from pmdarima import auto_arima
 from sensors.Cleanser import cleanser
@@ -41,7 +42,6 @@ class DataPrediction:
             current_date += self.prediction_intervals
             future_dates.append(current_date)
 
-        print(future_dates)
         self.X_future = future_dates
 
     def predict(self):
@@ -54,44 +54,14 @@ class DataPrediction:
 
         df = pd.DataFrame(data=self.Y, index=self.X, columns=['Value'])
 
-        auto_arima_model = auto_arima(df, seasonal=False, stepwise=True, suppress_warnings=True, error_action="ignore", max_order=None, trace=True)
+        auto_arima_model = auto_arima(df, seasonal=True, stepwise=True, suppress_warnings=True, error_action="ignore", trace=True)
 
         best_order = auto_arima_model.order
+        best_seasonal_order = auto_arima_model.seasonal_order
 
-        model = ARIMA(df, order=best_order)
+        model = SARIMAX(df, order=best_order, seasonal_order=best_seasonal_order)
 
         fitted_model = model.fit()
 
         self.model = fitted_model
-
-data = DataGeneration(9, 30, 1, dt.datetime(2023, 3, 15))
-
-X, Y = data.get_time_series()
-
-sensor = Sensor("Temperature Sensor", "Measures Temperature", X, Y)
-
-cleanse = cleanser(sensor, 2)
-
-cleanse.replace_missing_values()
-cleanse.set_data_types_to_float()
-      
-predict = DataPrediction(sensor, dt.datetime(2023, 4, 17))
-
-predict.set_prediction_timeframe()
-# predict.train_model()
-# predict.predict()
-
-# sensor.set_forecast_values(predict.forcasted_values)
-# sensor.set_forecast_date_range(predict.X_future)
-
-# print(sensor.get_forecast_values())
-# print(sensor.get_forecast_date_range())
-
-# plt.figure(figsize=(10, 5))
-# plt.plot(sensor.get_date_range(), sensor.get_value(), label='Actual Values')
-# plt.plot(sensor.get_forecast_date_range(), sensor.get_forecast_values(), label='Forecasted Values', linestyle='--')
-# plt.xlabel('Date')
-# plt.ylabel('Value')
-# plt.legend()
-# plt.show()
 
