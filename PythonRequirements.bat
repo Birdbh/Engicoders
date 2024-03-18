8b81f30cac3a26c2c85a48faa7745e63bd5619f2
Birdbh <birdlkjlkj123@gmail.com>
2024-03-17 19:49:41 -0700

    Updated with libraries for me and burkey

---
+++ PythonRequirements.bat
@@ -0,0 +1,6 @@
+py -m pip install python-engineio==4.9.0
+py -m pip install python-socketio==5.11.1
+py -m pip install statsmodels
+py -m pip install pandas
+py -m pip install scikit-learn
+py -m pip install pmdarima
