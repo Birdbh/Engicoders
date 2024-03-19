5321c62682db5e46ed2188d5990fb9950eac490e
JamesShadowfyre <88523672+JamesShadowfyre@users.noreply.github.com>
2024-03-19 15:07:43 -0700

    Create user.py

---
+++ src/flaskr/user.py
@@ -0,0 +1,14 @@
+from flask_login import UserMixin
+class User(UserMixin):
+    
+
+    
+    def User(): 
+        is_authenticated = False
+        is_active = False
+        is_anonymous = False
+        self.username = ""
+    def get_id():
+        return 0
+    def get_username():
+        return self.username
