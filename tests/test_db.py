a9dd337be1bd772c6806a0db98ca2c38c74ca53b
Birdbh <birdlkjlkj123@gmail.com>
2024-03-20 12:21:17 -0700

    Check out the change made to test_db.py that is stopping the workflow from running, I think it is resolved now

--- tests/test_db.py
+++ tests/test_db.py
@@ -25,7 +25,7 @@ def test_init_db_command(runner, monkeypatch):
    def fake_init_db():
        Recorder.called = True
   
-   monkeypatch.setattr('src.flaskr.db.init_db', fake_init_db())
+   monkeypatch.setattr('flaskr.db.init_db', fake_init_db())
    result = runner.invoke(args=['init-db'])
    assert 'initialized' in result.output
    assert Recorder.called
