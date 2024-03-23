c182ca15b8070f14264d1d5252c7ecccdcbf8df2
JamesShadowfyre <jamie.ropotar@gmail.com>
2024-03-23 11:59:50 -0700

    Update User class components

--- src/flaskr/user.py
+++ src/flaskr/user.py
@@ -1,14 +1,27 @@
 from flask_login import UserMixin
+import db
 class User(UserMixin):
-    
+    database = None
+    username = ""
+    userid = ""
+    is_authenticated = False
+    is_active = False
+    is_anonymous = False
 
-    
-    def User(): 
-        is_authenticated = False
-        is_active = False
-        is_anonymous = False
+    def User(self): 
+        self.database = db.get_db()
         self.username = ""
-    def get_id():
+        
+        
+
+    def get_id(self):
         return 0
-    def get_username():
+    def get_username(self):
         return self.username
+    def set_username(self, username):
+        self.username = username
+    def set_userid(self, userid): 
+        self.userid = database
+
+    def register(self, userid, username):
+        self.set_userid(self.database.execute("SELECT userid from user where username = (?)", (username,)))
