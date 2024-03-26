1d353de8d0b3862c931ca4f0c26467fc41e48182
brkykrclk <brky.krclk@gmail.com>
2024-03-25 19:14:45 -0700

    modified for charts

--- src/flaskr/home.py
+++ src/flaskr/home.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from datetime import datetime
+import json
 from flaskr import app
 import sys
 sys.path.append("src")
@@ -27,6 +28,8 @@ def home():
 
         # Initialize Sensor with fetched data and pass data to the template for rendering
         sensor = Sensor(name="Generated Sensor", description="Data from ThingSpeak", date_range=date_series, value=value_series)
+        labels_json=json.dumps(date_series)
+        values_json=json.dumps(value_series)
         return render_template('main/home.html', labels=date_series, values=value_series)
 
     return render_template('main/home.html')
