3e07565934572b2d8f4549123eb3e684d02a8487
JamesShadowfyre <jamie.ropotar@gmail.com>
2024-03-28 23:09:53 -0700

    Update user.py

--- src/flaskr/user.py
+++ src/flaskr/user.py
@@ -47,7 +47,7 @@ class SuperUser(User):
         self.hash_password()
 
     def hash_password(self):
-        hasher = sha512()
+        hasher = sha512() 
         password = str(self.database.execute("SELECT password from user where username = (?)", (self.username,)).fetchone()['password'])
         hasher.update(password.encode())
         self.password = hasher.hexdigest()
