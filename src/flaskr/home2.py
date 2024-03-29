2e2092c1f948e3f3befd3cf2276a3d67ba1e26a4
Birdbh <birdlkjlkj123@gmail.com>
2024-03-29 15:56:02 -0700

    Add Data upload functionality so that users can upload a csv and that data can be parsed as sensor data. Fixes #220

--- src/flaskr/home2.py
+++ src/flaskr/home2.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SubmitField, FileField, IntegerField, DateField, SelectField
+from wtforms import BooleanField, SubmitField, FileField, IntegerField, DateField, SelectField, StringField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from sensors import sensor, Prediction, Cleanser
 
 from DataGeneration import DataGeneration
 from DataUpload import DataUpload
@@ -13,20 +14,22 @@ from flask_login import login_user
 class HomeForm(FlaskForm):
     channel_id = IntegerField('Channel ID', validators=[DataRequired()])
     field_id = IntegerField('Field ID', validators=[DataRequired()])
-    start_date = DateField('Start Date', validators=[DataRequired()])
+    start_date = StringField('Start Date', validators=[DataRequired()])
     time_increment = IntegerField('Time Increment', validators=[DataRequired()])  # Modify choices as needed
     cleanse = BooleanField('Cleanse Data')
     predict = BooleanField('Predict Data')
-    prediction_date = DateField('Prediction Date')
+    prediction_date = StringField('Prediction Date')
     chartType = SelectField('Chart Type', choices=[('line', 'bar', 'radio')])  # Modify choices as needed
     stdDeviation = IntegerField('Standard Deviation')
     file = FileField('File Upload')
     submit = SubmitField('Submit')
 
     def get_time_series_data(self):
-        if self.file.filename != '':
+
+
+        if self.file.data.filename != '':
             try:
-                data_gen = DataUpload(self.file)
+                data_gen = DataUpload(self.file.data)
                 date_series, value_series = data_gen.get_time_series()
 
             except Exception as e:
@@ -35,8 +38,24 @@ class HomeForm(FlaskForm):
         else:
             # Initialize DataGeneration with form data
             try:
+
                 data_gen = DataGeneration(self.channel_id.data, self.time_increment.data, self.field_id.data, self.start_date.data)
                 date_series, value_series = data_gen.get_time_series()
 
             except Exception as e:
                 flash(f"Error while generating data: {e}")  # Use flash for error messages
+
+        return date_series, value_series
+
+    def apply_data_modifiers(self, sensor):
+        if self.cleanse.data:
+            sensor = Cleanser.cleanser(sensor, deviations=self.stdDeviation.data)
+
+        if self.predict.data:
+            sensor = Prediction.DataPrediction(sensor, self.prediction_date.data)
+
+        labels, values = sensor.process_data()
+        sensor.set_date_range(labels)
+        sensor.set_value(values)
+
+        return sensor
