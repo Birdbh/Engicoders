f0c7ff9dca81616d8d43ea0072ec6f862fb9e668
brkykrclk <brky.krclk@gmail.com>
2024-04-01 13:14:00 -0700

    Redesing Alarm and Alarm manager Fixes #218

--- src/sensors/alarm.py
+++ src/sensors/alarm.py
@@ -1,7 +1,5 @@
-import time
-
 class Alarm:
-    def __init__(self, sensor, threshold, deadband=0, delay=0, on_trigger=None):
+    def __init__(self, sensor, threshold, deadband=0, delay=0):
         self.sensor = sensor
         self.threshold = threshold
         self.deadband = deadband
@@ -9,7 +7,17 @@ class Alarm:
         self.is_set = False
         self.alarm_triggered = False
         self.trigger_time = None
-        self.on_trigger = on_trigger
+        self.observers = []  # List of observers to be notified upon alarm
+
+    def register_observer(self, observer):
+        self.observers.append(observer)
+
+    def unregister_observer(self, observer):
+        self.observers.remove(observer)
+
+    def notify_observers(self):
+        for observer in self.observers:
+            observer.update(self.sensor)
 
     def set_alarm(self):
         self.is_set = True
@@ -24,26 +32,21 @@ class Alarm:
     def check_sensor(self):
         if not self.is_set:
             return
-    
+
         current_value = self.sensor.get_value()
-    
+
         if current_value > self.threshold + self.deadband:
             if not self.alarm_triggered:
-                # Start the delay timer if it's not started yet
                 if self.trigger_time is None:
                     self.trigger_time = time.time()
-                # If there's no delay, or the delay has passed, trigger the alarm
                 if self.delay == 0 or time.time() - self.trigger_time >= self.delay:
                     self.trigger_alarm()
         else:
-            # Reset the trigger time if value falls below threshold or within deadband
             self.trigger_time = None
 
     def trigger_alarm(self):
         self.alarm_triggered = True
-        # Call the on_trigger callback
-        if self.on_trigger:
-            self.on_trigger(self.sensor)
+        self.notify_observers()
 
     def is_alarm_active(self):
         return self.alarm_triggered
